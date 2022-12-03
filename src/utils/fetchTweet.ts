f67bc678fc68741e475f7f34ad6b8029316634c2
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2022-12-03 12:11:18 -0300

    teste

--- src/utils/fetchTweet.ts
+++ src/utils/fetchTweet.ts
@@ -1,7 +1,7 @@
 import { Tweet } from "../../Typing"
 
 export const fetchTweets = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/getTweets`)
+  const res = await fetch(`https://twitter-clone-omega-drab.vercel.app/api/getTweets`)
 
   const data = await res.json()
   const tweets: Tweet[] = data.tweets;
